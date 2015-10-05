889578a4747e6e78b47098eaa80a3b86280f8b63
Sebastian <sep.gil@gmail.com>
2015-10-05 21:49:02 +0200

    Added ros setup.

---
+++ vagrant/setup.sh
@@ -0,0 +1,21 @@
+# Add ROS package server.
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+
+# Update package list.
+sudo apt-get update
+
+# Install ROS.
+sudo apt-get install ros-jade-desktop-full -y
+
+# Initilialize rosdep.
+sudo rosdep init
+rosdep update
+
+# Environment setup
+echo "source /opt/ros/jade/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+source /opt/ros/jade/setup.bash
+
+sudo apt-get install python-rosinstall -y
